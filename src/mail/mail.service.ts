be379dfaef80f4e274102791cd480f7f9987f03b
promarat <ace1998116@gmail.com>
2023-05-04 23:52:53 +0300

    s

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -79,6 +79,9 @@ export class MailsService {
             if (res?.failureCount) {
            //     console.log(res.results?.[0]?.error?.message,"__________________________________");
             }
+            for(let i = 0 ; i < res.results.length; i ++){
+                console.log(res.results[i],":::i");
+            }
             // return error message if any error create in fcm process
         //    console.log(res.results?.[0]?.messageId,"---------------------------------------");
         } catch (error) {
