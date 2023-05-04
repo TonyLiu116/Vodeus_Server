4449b0c4757165b09e96903b2532c0fe3dd6ad63
promarat <ace1998116@gmail.com>
2023-05-04 21:20:01 +0300

    s

--- src/actions/actions.service.ts
+++ src/actions/actions.service.ts
@@ -1029,10 +1029,10 @@ export class ActionsService {
   }
 
   async createBirdRoom(user, roomId) {
-    const findUsers = await this.findUsersByFriendId(user.id);
-    if (findUsers.length == 0)
-      return;
-    const usersId = findUsers.map((user) => user.user.id);
+    // const findUsers = await this.findUsersByFriendId(user.id);
+    // if (findUsers.length == 0)
+    //   return;
+    // const usersId = findUsers.map((user) => user.user.id);
     const findUser = await this.usersRepository.findOne({ where: { id: user.id } });
     let description = {
       eg: `${findUser.name} has created a live room, join now ! 👀`,
